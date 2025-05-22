3c7a48ff3b99638276515f60ae3740c57de11494
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-05-22 14:59:44 +0200

    costomiezed most used components

--- multitenant-ecommerce/src/app/page.tsx
+++ multitenant-ecommerce/src/app/page.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello world</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am button</Button>
+        </div>
+        <div>
+          <Input placeholder="i am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="This is the textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
