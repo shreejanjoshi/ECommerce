92c2b9ea7f16b32a1fa560814d701f8eaae11aad
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-06-02 14:27:27 +0200

    install and used payload integration

---
+++ multitenant-ecommerce/src/app/(app)/(home)/page.tsx
@@ -0,0 +1,19 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import { json } from 'stream/consumers';
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+    const data = await payload.find({
+    collection: 'categories',
+  })
+
+  return (
+    <div>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
+}
